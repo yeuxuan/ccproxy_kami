2199cc94a05c506ab656a2785a22b4db0a0d4294
kaishimm <108939472+kaishimm@users.noreply.github.com>
2022-07-08 21:38:02 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
         /*数据库配置*/
         $dbconfig=array(
-	'host' => 'localhost', //数据库服务器
+	'host' => 'laoshumm', //数据库服务器
 	'port' => 3306, //数据库端口
 	'user' => 'ccpy', //数据库用户名
 	'pwd' => '123456z', //数据库密码
